47423c4e381e230ccdb8974f9e02be5b80c17b51
Ali Yusuf <aliateeq95@gmail.com>
2020-04-10 23:32:28 +0300

    add includeBase for profile mapping

--- src/app/models/profile.model.ts
+++ src/app/models/profile.model.ts
@@ -1,7 +1,8 @@
-import {Mapper} from '@nartc/automapper';
+import {ignore, Mapper} from '@nartc/automapper';
 import {ProfileDto} from '../dto/profile.dto';
 import {BaseProfileModel} from './base-profile.model';
+import {BaseProfileDto} from '../dto/base-profile.dto';
 
 export class ProfileModel extends BaseProfileModel {}
 
-Mapper.createMap(ProfileDto, ProfileModel);
+Mapper.createMap(ProfileDto, ProfileModel, {includeBase: [BaseProfileDto, BaseProfileModel]});
